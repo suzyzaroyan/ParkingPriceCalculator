673079392df0c860115ff3bef7293b6c1cba930c
suzyzaroyan <62140748+suzyzaroyan@users.noreply.github.com>
2020-04-23 22:04:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function checkentry(evt) {
+  var time =parseFloat(evt.currentTarget.value);
+
+  if (time > 24 || time < 1) {
+    console.log("Wrong value")
+    evt.currentTarget.style.color = "red"
+  } else {
+    console.log("Correct Value")
+    evt.currentTarget.style.color = "black"
+  }
+
+  function calculatePrice() {
+
+   var price1 = parseFloat(document.getElementById("price1").value)
+
+
+   var currentprice = price1 * 100
+  console.log(currentPrice)
+
+  document.getElementById("CurrentPriceVAL").innerHTML = currentprice
+
+  }
