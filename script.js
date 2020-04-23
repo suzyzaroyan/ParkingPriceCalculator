d01dbb661c405cd08fd40ca5c34c7c82fc6f9789
suzyzaroyan <62140748+suzyzaroyan@users.noreply.github.com>
2020-04-23 22:08:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function checkentry(evt) {
   var time =parseFloat(evt.currentTarget.value);
 
   if (time > 24 || time < 1) {
-    console.log("Wrong value")
-    evt.currentTarget.style.color = "red"
+    console.log("Wrong value");
+    evt.currentTarget.style.color = "red";
   } else {
-    console.log("Correct Value")
-    evt.currentTarget.style.color = "black"
+    console.log("Correct Value");
+    evt.currentTarget.style.color = "black";
   }
 
   function calculatePrice() {
 
-   var price1 = parseFloat(document.getElementById("price1").value)
+   var price1 = parseFloat(document.getElementById ("price1").value);
 
 
-   var currentprice = price1 * 100
-  console.log(currentPrice)
+   var currentprice = price1 * 100;
 
-  document.getElementById("CurrentPriceVAL").innerHTML = currentprice
+
+    document.getElementById("CurrentPriceVAL").innerHTML = currentprice;
 
   }
